e351dc69211ab168f1c96abbf286f50659bc0b47
shaikdavoodbasha <shannushaik080@gmail.com>
2026-04-21 10:01:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import HomePage from './pages/HomePage'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{textAlign:'center',paddingTop:'29px',paddingBottom:'29px',backgroundColor:'#520FB7',height:'112px'}}>
+      <h1 style={{fontSize:'32px',fontWeight:'bold',color:"#FFFFFF"}}>Pokemon Listing</h1>
+    </div>
+  )
+}
+
+export default Header
