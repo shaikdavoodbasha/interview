0b155f801c50b084ba98f949a3b57ec405b3fa7b
shaikdavoodbasha <shannushaik080@gmail.com>
2026-04-26 17:38:41 +0530

    vi1

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,65 +1,65 @@
-import React from "react";
+// import React from "react";
 
-const Card = ({ name }) => {
-  return (
-    <div
-      style={{
-        width: "167px",
-        height: "227px",
-        backgroundColor: "#FFFAEB",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: "8px",
-        paddingLeft: "32px",
-        paddingRight: "32px",
-        paddingTop: "24px",
-        paddingBottom: "24px",
-        borderRadius: "16px",
-        boxShadow: "0px 2px 3px 0px #D9D9D9",
-      }}
-    >
-      <div
-        style={{
-          width: "87px",
-          height: "88px",
-          borderRadius: "56px",
-          backgroundColor: "#D9D9D9",
-        }}
-      ></div>
-      <h1
-        style={{
-          paddingLeft: "3px",
-          paddingRight: "3px",
-          paddingTop: "2px",
-          paddingBottom: "2px",
-          fontSize: "16px",
-          color: "#2A085E",
-          backgroundColor: "#F0E7FD",
-          borderRadius: "6.4px",
-        }}
-      >
-        #55
-      </h1>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "2px",
-          alignItems: "center",
-        }}
-      >
-        <h1 style={{ fontSize: "16px", fontWeight: "700", color: "#292929" }}>
-          {name}
-        </h1>
-        <p
-          style={{ fontSize: "12px", color: "Type: Grass", fontWeight: "500" }}
-        >
-          Type: Grass
-        </p>
-      </div>
-    </div>
-  );
-};
+// const Card = ({ name }) => {
+//   return (
+//     <div
+//       style={{
+//         width: "167px",
+//         height: "227px",
+//         backgroundColor: "#FFFAEB",
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         gap: "8px",
+//         paddingLeft: "32px",
+//         paddingRight: "32px",
+//         paddingTop: "24px",
+//         paddingBottom: "24px",
+//         borderRadius: "16px",
+//         boxShadow: "0px 2px 3px 0px #D9D9D9",
+//       }}
+//     >
+//       <div
+//         style={{
+//           width: "87px",
+//           height: "88px",
+//           borderRadius: "56px",
+//           backgroundColor: "#D9D9D9",
+//         }}
+//       ></div>
+//       <h1
+//         style={{
+//           paddingLeft: "3px",
+//           paddingRight: "3px",
+//           paddingTop: "2px",
+//           paddingBottom: "2px",
+//           fontSize: "16px",
+//           color: "#2A085E",
+//           backgroundColor: "#F0E7FD",
+//           borderRadius: "6.4px",
+//         }}
+//       >
+//         #55
+//       </h1>
+//       <div
+//         style={{
+//           display: "flex",
+//           flexDirection: "column",
+//           gap: "2px",
+//           alignItems: "center",
+//         }}
+//       >
+//         <h1 style={{ fontSize: "16px", fontWeight: "700", color: "#292929" }}>
+//           {name}
+//         </h1>
+//         <p
+//           style={{ fontSize: "12px", color: "Type: Grass", fontWeight: "500" }}
+//         >
+//           Type: Grass
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Card;
+// export default Card;
